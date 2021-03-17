bcf55b82a9fd6731f5db6037a33beedeab16251a
Zhikai Luo <luozk@keenon.com>
2021-03-17 18:34:25 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-    <div id="hello">hello</div>
+    <div>
+        <div id="hello">{{mynum}}</div>
+        <test :user="Puser" :pro.sync="mynum">
+            <template #s1="{user}">
+                <!-- #:v-slot的缩写   =：作用域插槽-->
+                {{user.first}}
+            </template>
+            hello{{we}}
+        </test>
+    </div>
 </template>
 <script>
+import test from './Idx.vue'
 export default {
-    
+    data(){
+        return {
+            we:'hello',
+            Puser:{
+                last:"luo",
+                first:"lizz"
+            },
+            mynum:1
+        }
+    },
+    components:{
+        test
+    }
 }
 </script>
 <style>
---
+++ src/Idx.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d">
+        <!-- 双向绑定： -->
+        <button @click="$emit('update:pro',pro+1)">update</button>
+        <slot name="s1" :user="user">{{user.last}}</slot>
+        <slot/>
+    </div>
+</template>
+<script>
+export default {
+    props:['user','pro']
+}
+</script>
+<style>
+.d{
+    width: 200px;
+    height: 100px;
+    background-color: aquamarine;
+}
+</style>
